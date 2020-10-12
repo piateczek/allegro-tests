b9631d941cc8e60bf648175b85d01c035576fd89
Michal Piatek <michal.piatek.email@gmail.com>
2020-10-12 15:58:46 +0200

    ISSUE_005: Code refactoring

--- src/test/kotlin/com/example/allegroRESTAPItests/models/MainErrors.kt
+++
@@ -1,8 +0,0 @@
-package com.example.allegroRESTAPItests.models
-
-import com.google.gson.annotations.SerializedName
-
-data class MainErrors(
-        @SerializedName("errors")
-        val errors: List<Error>
-)
